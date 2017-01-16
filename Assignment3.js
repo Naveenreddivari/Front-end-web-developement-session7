e2d8cf27740ae15df641878ca59e562dca0a19a3
Naveenreddivari <naveenreddyvari@gmail.com>
2017-01-16 16:12:03 +0530

    Session7-Assignment3
    Assignment3

---
+++ Assignment3.js
@@ -0,0 +1,15 @@
+var counter = 0;
+
+function Counter() {
+    return counter += 1;
+}
+console.log(Counter());
+console.log(Counter());
+console.log(Counter());
+console.log(Counter());
+console.log(Counter());
+console.log(Counter());
+console.log(Counter());
+console.log(Counter());
+console.log(Counter());
+console.log(Counter());
